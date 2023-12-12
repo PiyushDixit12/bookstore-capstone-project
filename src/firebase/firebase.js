da61a6af70649b46f30e02349c6e621a4e9b5a86
Piyush Dixit <piyushd.bca2022@ssism.org>
2023-12-12 23:58:13 +0530

    added a login

---
+++ src/firebase/firebase.js
@@ -0,0 +1,25 @@
+
+import firebase from 'firebase/compat/app'
+import {getAuth,GoogleAuthProvider} from 'firebase/auth'
+import "firebase/compat/firestore"
+// npm install firebass
+// npm install -g firebase-tools
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBqV9B2ft5aqtOXhYtq9Pc8S-PLfO5vvVE",
+    authDomain: "capstone-project-bookstore.firebaseapp.com",
+    projectId: "capstone-project-bookstore",
+    storageBucket: "capstone-project-bookstore.appspot.com",
+    messagingSenderId: "1064010249013",
+    appId: "1:1064010249013:web:5424b0e200bbf9c791927c"
+};
+
+
+// Initialize Firebase connect to firebase
+const app = firebase.initializeApp(firebaseConfig);
+// connect to firestore database 
+export const myDataBase = firebase.firestore();
+
+// GoogleAuthProvider class helep to connect google auth provider
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();          
